7ffb739686aae563eecba2acedf4f5e060b791a4
Anton Lisovski <lisovskijj.99@gmail.com>
2022-12-11 23:51:00 +0300

    I

---
+++ src/main/java/RegistrationServlet.java
@@ -0,0 +1,33 @@
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+
+@WebServlet("/registration")
+public class Registration extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        Model model =Model.getInstance();
+        List<String> logins = model.list();
+        req.setAttribute("log",logins);
+        RequestDispatcher requestDispatcher = req.getRequestDispatcher("/listShow.jsp");
+        requestDispatcher.forward(req,resp);
+    }
+
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        String login = req.getParameter("login");
+        String password = req.getParameter("password");
+        getServletContext().getRequestDispatcher("/my-registration.jsp").forward(req, resp);
+        User user = new User(login, password);
+        Model model = Model.getInstance();
+        model.add(user);
+    }
+}
